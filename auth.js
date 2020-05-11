08e8b609e29bf9f0f9501c1c0c82d69bde496658
Andrew Thompson <ioota@icloud.com>
2020-05-11 11:43:23 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -103,7 +103,7 @@ function authCheck (destURL, userEntry) {
     // return true;
     return true;
   } else {
-    console.log("something went wrong");
+    console.log("something went wrong; authstate=" + checkCookie("authState"));
     return false;
     // on page, clear form entry and display text "Password Incorrect"
   }
